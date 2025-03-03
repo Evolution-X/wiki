148e7933171c3b51f80ebe39a24d322ee7b79105
Turtlepaw <81275769+Turtlepaw@users.noreply.github.com>
2025-03-03 18:13:19 -0500

    wiki: metadata: introduce metadata
    * Introduce Metadata
    * Imrove documentation
    
    * feat: update documentation and layout components for Evolution X Wiki
    * fix: metadata
    * fix: add metadata base and update footer
    * fix: improve metadata
    * fix: add .node-version
    * docs: update contributing guide and readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,36 @@ import { getPageMap } from "nextra/page-map";
 import "nextra-theme-docs/style.css";
 import "material-symbols";
 import "../styles.css";
+import type { Metadata } from "next";
+import type { PropsWithChildren } from "react";
 
-export const metadata = {
-  // Define your metadata here
-  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+export const metadata: Metadata = {
+  metadataBase: new URL("https://wiki.evolution-x.org"),
+  title: "Evolution X Wiki",
+  description: "The official wiki for Evolution X.",
+  icons: {
+    icon: "/icon.svg",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://wiki.evolution-x.org",
+    siteName: "Evolution X Wiki",
+    title: "Evolution X Wiki",
+    description: "The official wiki for Evolution X.",
+    determiner: "the",
+    locale: "en_US",
+    images: {
+      url: "/keepevolving.png",
+      width: 4888,
+      height: 1622,
+      alt: 'Evolution X\'s Banner "#KeepEvolving"',
+      type: "image/png",
+    },
+  },
 };
 
 const banner = (
-  <Banner storageKey="some-key">Evolution X 10 released 🎉</Banner>
+  <Banner storageKey="evolution-x-10-3">Evolution X 10.3 released 🎉</Banner>
 );
 const logo = (
   <svg
@@ -42,9 +64,9 @@ const navbar = (
     // ... Your additional navbar options
   />
 );
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>;
+const footer = <Footer>#KeepEvolving</Footer>;
 
-export default async function RootLayout({ children }) {
+export default async function RootLayout({ children }: PropsWithChildren) {
   return (
     <html
       // Not required, but good for SEO
@@ -57,17 +79,16 @@ export default async function RootLayout({ children }) {
       <Head
       // ... Your additional head options
       >
-        <link rel="icon" href="/icon.svg" />
         {/* Your additional tags should be passed as `children` of `<Head>` element */}
       </Head>
       <body>
         <Layout
-          //banner={banner}
+          //banner={banner} // uncomment to show optional banner
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          docsRepositoryBase="https://github.com/Evolution-X/wiki/tree/main"
           footer={footer}
-          // ... Your additional layout options
+          darkMode
         >
           {children}
         </Layout>
