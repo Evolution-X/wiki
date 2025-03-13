308b87c50ea748351eda59fbbca3869a8904e918
0xSoul <31786518+0xSoul24@users.noreply.github.com>
2025-03-12 20:51:33 -0500

    Custom OTA initial draft

--- content/_meta.js
+++ content/_meta.js
@@ -10,6 +10,9 @@ export default {
   "setting-up-env-latest-lts": (
     <Entry title="Setting up environment latest LTS" icon="manufacturing" />
   ),
+  "setting-up-custom-ota": (
+    <Entry title="Setting up custom OTA" icon="update" />
+  ),
   lunch: <Entry title="Lunch guide" icon="restaurant" />,
   build_flags: <Entry title="Build flags" icon="flag" />,
   "reporting-a-bug": <Entry title="Reporting a bug" icon="bug_report" />,
